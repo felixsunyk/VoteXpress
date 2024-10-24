8ff4ed2ae8e3a34964f277c611af4eb9eb7a98a3
felixsunyk <felixkpormon@gmail.com>
2024-10-24 18:00:28 +0100

    added countdown time

--- src/component/voter/Instruction.js
+++ src/component/voter/Instruction.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../style/instruction.css";
 import { Link } from "react-router-dom";
+import CountdownTimer from "./CountdownTime";
 
 
 const Instruction = () =>{
@@ -19,9 +20,9 @@ const Instruction = () =>{
             <p><span>Choose Carefully:</span> Once a vote is cast, it cannot be changed, so make sure you're confident in your decision.</p>
             <p><span>Stay Informed:</span> Results will be available in real-time after the vote.</p>
             <div className="instruction-btn">
-                <button>{date.toDateString()}</button>
-                
-                <button>{futureDate.toDateString() }</button>
+              
+                <CountdownTimer />
+              
             </div>
             <div>
                  <Link to="/Voting" className="btn">start voting</Link>
@@ -32,46 +33,4 @@ const Instruction = () =>{
 }
 export default Instruction;
 
-// import React, { useState, useEffect } from "react";
 
-// const CountdownTimer = () => {
-//   // Set your target date and time (e.g., 3 days from now)
-//   const targetDate = new Date().getTime() + 3 * 24 * 60 * 60 * 1000; // 3 days from now
-
-//   const [timeLeft, setTimeLeft] = useState(targetDate - new Date().getTime());
-
-//   useEffect(() => {
-//     const intervalId = setInterval(() => {
-//       const now = new Date().getTime();
-//       const timeRemaining = targetDate - now;
-
-//       if (timeRemaining <= 0) {
-//         clearInterval(intervalId);
-//         setTimeLeft(0); // Countdown reached zero
-//       } else {
-//         setTimeLeft(timeRemaining);
-//       }
-//     }, 1000); // Update every second
-
-//     return () => clearInterval(intervalId); // Cleanup on component unmount
-//   }, [targetDate]);
-
-//   const formatTimeLeft = (milliseconds) => {
-//     const totalSeconds = Math.floor(milliseconds / 1000);
-//     const days = Math.floor(totalSeconds / (60 * 60 * 24));
-//     const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
-//     const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
-//     const seconds = totalSeconds % 60;
-
-//     return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-//   };
-
-//   return (
-//     <div>
-//       <h2>Countdown Timer</h2>
-//       <p>{timeLeft > 0 ? formatTimeLeft(timeLeft) : "Time's up!"}</p>
-//     </div>
-//   );
-// };
-
-// export default CountdownTimer;
